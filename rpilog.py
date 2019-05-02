019661962919f6b5d4d2d935c16e5a067d031eb0
Lucas Oshiro <lucasseikioshiro@gmail.com>
2019-05-02 10:22:48 -0300

    Output csv
    Signed-off-by: Lucas Oshiro <lucasseikioshiro@gmail.com>

--- rpilog.py
+++ rpilog.py
@@ -4,11 +4,27 @@ from sys import argv
 from time import sleep
 from monitor import Monitor
 
-def main():
-    monitor = Monitor(*argv[1:])
+def parse_args(args):
+    options = {
+        '-t': 'temp',
+        '-c': 'usage',
+        '-p': 'power'
+    }
+    return tuple(options[arg] for arg in args)
 
-    while True:
-        print(monitor.all())
-        sleep(1)
+def main():
+    options = parse_args(argv[1:])
+    monitor = Monitor(*options)
+
+    print(*options, sep=',')
+
+    try:
+        while True:
+            sample = monitor.all()
+            print(*(sample[op] for op in options), sep=',')
+            sleep(0.2)
+
+    except KeyboardInterrupt:
+        exit(0)
 
 if __name__ == '__main__': main()
