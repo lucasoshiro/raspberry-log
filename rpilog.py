3c8af67b3da8bc44f0d5df5d9683e8fd61c2b76e
Lucas Oshiro <lucasseikioshiro@gmail.com>
2019-06-25 01:01:16 -0300

    Add cleaner flag parser
    Signed-off-by: Lucas Oshiro <lucasseikioshiro@gmail.com>

--- rpilog.py
+++ rpilog.py
@@ -4,28 +4,39 @@ from sys import argv
 from time import sleep
 from monitor import Monitor
 
+opt_table = [
+    ('h',  'help',     ('help',),              'show help'),
+    ('t',  'temp',     ('temp',),              'log CPU temperature'),
+    ('c',  'cpu',      ('usage',),             'log CPU usage'),
+    ('p',  'power',    ('power',),             'log Raspberry Pi power comsumption'),
+    ('m',  'mem',      ('ram',),               'log RAM usage'),
+    (None, 'net_down', ('net_down',),          'log download rate'),
+    (None, 'net_up',   ('net_up',),            'log upload rate'),
+    ('n',  None,       ('net_down', 'net_up'), 'log both download and upload rates'),
+]              
+
 def parse_args(args):
-    opt_table = {
-        '-h': 'help',
-        '-t': ('temp',),
-        '-c': ('usage',),
-        '-p': ('power',),
-        '-m': ('ram',),
-        '-n': ('net_down', 'net_up'),
+    flags = [*filter(lambda arg: arg[0] == '-', args)]
 
-        '--help':     ('help',),
-        '--temp':     ('temp',),
-        '--cpu':      ('usage',),
-        '--power':    ('power',),
-        '--mem':      ('ram',),
-        '--net_down': ('net_down',),
-        '--net_up':   ('net_up',),
-    }
+    simple = filter(lambda flag: len(flag) >= 2 and flag[1] != '-', flags)
+    simple = list(''.join(x[1:] for x in simple))
 
-    options = tuple(opt
-        for opt_t in filter(lambda arg: arg is not None, (opt_table.get(arg) for arg in args))
-        for opt in opt_t)
+    double = filter(lambda flag: len(flag) >= 3 and flag[1] == '-', flags)
+    double = [x[2:] for x in double]
+
+    
     logfilename = args[-1] if args and args[-1][0] != '-' else None
+
+    simple_opt = {flag: opt for flag, _, opt, __ in opt_table}
+    double_opt = {flag: opt for _, flag, opt, __ in opt_table}
+
+    options = [
+        *(simple_opt[flag] for flag in simple),
+        *(double_opt[flag] for flag in double)
+    ]
+
+    options = [e for t in options for e in t]
+
     return options, logfilename
 
 def show_help():
@@ -38,32 +49,23 @@ def show_help():
             rpilog [OPTIONS] FILE
 
     OPTIONS
-            -h, --help
-                    show help
-
-            -t, --temp
-                    log CPU temperature
-
-            -c, --cpu
-                    log CPU usage
-
-            -p, --power
-                    log Raspberry Pi power comsumption
-
-            -m, --mem
-                    log RAM usage
-
-            --net_down
-                    log download rate
-
-            --net_up
-                    log upload rate
-
-            -n, --net
-                    log both download and upload rates
+{}
     """
 
-    print(help_str); exit(0)
+    opt_str = """
+           {}
+                   {}
+"""
+
+    print(help_str.format(
+        '\n'.join(
+            opt_str.format(
+                ('-{} '.format(opt[0]) if opt[0] else '') +
+                ('--{}'.format(opt[1]) if opt[1] else ''),
+                opt[3])
+            for opt in opt_table
+        )))
+    exit(0)
 
 def main():
     options, logfilename = parse_args(argv[1:])
